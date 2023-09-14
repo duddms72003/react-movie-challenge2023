6829255e9516eb17a5b9c8e68cae34b85d06d05d
Jane <duddms72003@naver.com>
2023-09-14 20:30:41 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`/`} element={<Home />} />
         <Route path="/character/:id" element={<Detail />} />
       </Routes>
     </Router>
